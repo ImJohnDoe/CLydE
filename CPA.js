e0fa8a6f9b963e9019c3baa362dcba735a0e89b6
ImJohnDoe <Pshichenko@hotmail.com>
2014-02-08 17:34:30 -0800

    Update CPA.js

--- CPA.js
+++ CPA.js
@@ -1,15 +1,15 @@
 simply.body("", true);
 
 
-simply.title('CLydE');
+simply.title('       CLydE');
 
 var time = new Date();
 var hour = time.getHours(); 
 var minute = time.getMinutes();
 
-/*
-simply.text(hour + ":" + minute);
 
+simply.text(hour + ":" + minute);
+/*
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
