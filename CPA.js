92ffd132debf2e0cc358ca62ed16bda198e948b5
ImJohnDoe <Pshichenko@hotmail.com>
2014-02-08 18:12:08 -0800

    Update CPA.js

--- CPA.js
+++ CPA.js
@@ -1,7 +1,7 @@
 simply.body("", true);
 
 
-simply.title('             CLydE');
+simply.title('        CLydE');
 
 var time = new Date();
 var hour;
@@ -12,16 +12,46 @@ if(time.getHours() > 12){
 }
 var minute = time.getMinutes();
 
-simply.subtitle("                                         " + hour + ":" + minute);
+if (hour < 10) {
+  simply.subtitle("                                 0" + hour + ":" + minute);
+} else {
+  simple.subtitle("                                  " + hour + ":" + minute);
+}
 
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
-    minute += 1;
-    simply.subtitle("                                    " + hour + ":" + minute);
+    if(minute === 59) {
+      minute = 0;
+        if (hour != 12) {
+          hour += 1;
+        } else {
+          hour = 1;
+        }
+    } else {
+      minute += 1;
+    }
+    if (hour < 10) {
+      simply.subtitle("                                 0" + hour + ":" + minute);
+    } else {
+      simple.subtitle("                                  " + hour + ":" + minute);
+    }
   } else if (e.button === 'down') {
-    minute -= 1;
-    simply.subtitle("                                    " + hour + ":" + minute);
+    if (minute === 0) {
+      minute = 59;
+       if (hour != 1) {
+          hour -= 1;
+        } else {
+          hour = 12;
+        }
+      } else {
+        minute -= 1;
+      }
+    if (hour < 10) {  
+      simply.subtitle("                                 0" + hour + ":" + minute);
+    } else {
+      simple.subtitle("                                  " + hour + ":" + minute);
+    }
   }
 });
 
